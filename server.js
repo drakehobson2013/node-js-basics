57a2633b1bdd362b4ca9f9c9eb3658c586d8da95
Drake Hobson <drakehobson2013@gmail.com>
2021-03-01 10:51:36 -0500

    day 3

--- server.js
+++ server.js
@@ -1,48 +1,58 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+// const _ = require('lodash');
 
-const server = http.createServer((req, res) => {
-console.log(req.url, req.method);
+// const server = http.createServer((req, res) => {
+// // lodash
+// const num = _.random(0,20);
+// console.log(num);
 
-// set header content type
-res.setHeader('Content-Type', 'text/html');
+// const greet = _.once(() => {
+//     console.log('hello');
+// });
 
-let path = './views/';
-switch(req.url) {
-    case '/':
-        path += 'index.html';
-        res.statusCode = 200;
-        break;
-    case '/about':
-        path += 'about.html';
-        res.statusCode = 200;
-        break;
-    case '/about-me':
-        res.statusCode = 301;
-        res.setHeader('Location', '/about');
-        res.end();
-        break;
-    default:
-        path += '404.html';
-        res.statusCode = 404;
-        break;    
-}
+// greet();
+// greet();
 
-// send an html file
-fs.readFile(path, (err, data) => {
-    if(err){
-        console.log(err);
-        res.end();
-    }
-    else{
-        // res.write(data);
+// // set header content type
+// res.setHeader('Content-Type', 'text/html');
+
+// let path = './views/';
+// switch(req.url) {
+//     case '/':
+//         path += 'index.html';
+//         res.statusCode = 200;
+//         break;
+//     case '/about':
+//         path += 'about.html';
+//         res.statusCode = 200;
+//         break;
+//     case '/about-me':
+//         res.statusCode = 301;
+//         res.setHeader('Location', '/about');
+//         res.end();
+//         break;
+//     default:
+//         path += '404.html';
+//         res.statusCode = 404;
+//         break;    
+// }
+
+// // send an html file
+// fs.readFile(path, (err, data) => {
+//     if(err){
+//         console.log(err);
+//         res.end();
+//     }
+//     else{
+//         // res.write(data);
         
-        res.end(data);
-    }
-})
+//         res.end(data);
+//     }
+// })
 
-});
+// });
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for requests on port 3000');
-});
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening for requests on port 3000');
+// });
